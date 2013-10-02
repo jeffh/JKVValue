6e20ff36583c25b7fae1fc40bb80f8810416ff30
Jeff Hui <jeff@jeffhui.net>
2013-10-02 19:29:29 -0400

    Initial Commit

---
+++ JKValue/JKValue.h
@@ -0,0 +1,13 @@
+//
+//  JKValue.h
+//  JKValue
+//
+//  Created by Jeff Hui on 10/2/13.
+//  Copyright (c) 2013 Jeff Hui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JKValue : NSObject
+
+@end
---
+++ JKValue/JKValue.m
@@ -0,0 +1,13 @@
+//
+//  JKValue.m
+//  JKValue
+//
+//  Created by Jeff Hui on 10/2/13.
+//  Copyright (c) 2013 Jeff Hui. All rights reserved.
+//
+
+#import "JKValue.h"
+
+@implementation JKValue
+
+@end
